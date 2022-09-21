5c9541833b3724fcb31edea1fffd642789d5ae72
TamunaI <tamuna.iosava@gmail.com>
2022-09-21 20:40:58 +0400

    first commit

---
+++ day1/lesson1.py
@@ -0,0 +1,13 @@
+name = "Tamuna" #string
+surname = "Iosava" #string
+age = 36 #integer
+height = 167.5 #float
+about = "has just started with Python"
+info = "she is"
+knows_programming = False #boolean
+is_smart = True #boolean
+
+print(name + " " + (surname) + " " + (about) + " " + (info) + " " + str(age) + " " + (info) + " " + str(height) )
+
+
+
