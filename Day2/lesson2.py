702fe716d239cc60633526375a1655d5dfd23fc8
TamunaI <tamuna.iosava@gmail.com>
2022-09-22 20:56:08 +0400

    lesson 2

---
+++ Day2/lesson2.py
@@ -0,0 +1,27 @@
+name = "tamuna"
+surname = "Iosava"
+age = 36
+height = 167.5
+
+print("my name is {} and my age is {}" .format (name, age))
+print(name.capitalize())
+
+full_name = "tamuna iosava"
+
+print (full_name[3:8])
+if "m" in full_name:
+    print("aris")
+
+print(full_name[2:])
+print(full_name[:9])    
+print(full_name[::2])
+print(full_name[-1]) 
+print(full_name[9:3:-1])   
+print(full_name.upper())
+full_name = "    Tamuna   "
+print(full_name.strip())
+
+full_name = "Tamuna Iosava"
+print(full_name.replace("T", "K"))
+
+
