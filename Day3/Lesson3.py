c4390f512705965de6e4eb39ed360aa26e6cd2a0
TamunaI <tamuna.iosava@gmail.com>
2022-10-03 21:08:35 +0400

    4th commit

---
+++ Day3/Lesson3.py
@@ -0,0 +1,53 @@
+
+
+# my_age = input("how old are you: ")
+# print("I am " + my_age + "years old")
+
+# num1 = int(input("enter number1: "))
+# num2 = int(input("enter number2: "))
+# print("the result is " + str(num1*num2))
+# product = num1*num2
+# if product >100:
+#     print("xxx")
+# else:
+#     print("yyy")    
+
+
+ 
+    # print(a+b)
+    # print(a*b)
+    # print(a/b)
+    # print(a-b)
+    # print(b%a) ramdeni darcheba nashti gayofis mere
+    # print(a**b) xarisxshi ayvana
+    # print(a//b)
+
+
+a = int(input("enter number1: "))
+b = int(input("enter number2: "))
+c = int(input("enter number3: "))
+
+# 20, 13, 5
+my_sum = 0
+
+if a %2 ==1:
+    my_sum += a
+if b %2 ==1:
+    my_sum += b
+if c %2 ==1:
+    my_sum += c
+
+print("the sum of odd numbers is {}". format(my_sum))
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day4/lesson4.py
@@ -0,0 +1,51 @@
+# momxmarebels shemoatanine ori saxeli da is damiprintre 
+# romelshic meti xmovani iqneba
+
+# symbol in "aeiou"
+
+# name1 = input("enter name1: ")
+# name2 = input("enter name2: ")
+
+# ammount_of_vowels_in_name1 = 0
+# ammount_of_vowels_in_name2 = 0
+
+# for char in name1:
+#     if char in "aeiou":
+#         ammount_of_vowels_in_name1 +=1
+
+# for char in name2:
+#     if char in "aeiou":
+#         ammount_of_vowels_in_name2 +=1
+
+# if ammount_of_vowels_in_name1 > ammount_of_vowels_in_name2:
+#     print("the ammount of vowels in name1 is more and it contains {} vowels".format(ammount_of_vowels_in_name1))
+# elif ammount_of_vowels_in_name1 < ammount_of_vowels_in_name2:
+#     print("the ammount of vowels in name2 is more and it contains {} vowels".format(ammount_of_vowels_in_name2))
+# else:
+#     print("they have equal amount of vowels")
+
+# name1 = "Tamuna"
+# name2 = "Avtandili"
+
+# if len(name1) > len(name2):
+#     print("pirveli ufro grdzelia")
+# elif len(name1) < len(name2):
+#     print("meore ufro grdzelia")
+# else:
+#     print("tolia")
+
+print(5 != 10)
+print("A" not in "jhfglkj")
+
+
+
+
+# my_name = "Tamuna"
+
+# for char in my_name:
+#     print(char)
+
+# for i in range(10):
+#     print(str(1) + "a")
+    
+# char da i saiteracio cvladebia am shemtxvevashi. iteration - brunva
