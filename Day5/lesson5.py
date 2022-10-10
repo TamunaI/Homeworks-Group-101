55ae72aa6198e3c37953ede3c92e6ea2125444cb
TamunaI <tamuna.iosava@gmail.com>
2022-10-10 19:31:06 +0400

    first commit

---
+++ Day5/lesson5.py
@@ -0,0 +1,69 @@
+# # for loop
+# # while loop
+
+# # # i saiteracio cvladi
+
+# # for i in range(3, 6):
+# #     print("tamuna")
+
+# #                 # start, finish, step
+# # # for x in range(5, 10,2): range 5 -10 (5, 7,9)
+# #     print(str(x) + "nika")
+
+# i = 0
+# while i <5:
+#     print("Tamuna")
+#     i +=1
+
+# full_name = "Tamuna Iosava"
+
+# i = 0
+# while i < len(full_name):
+#     print(full_name[i])
+#     i +=1
+
+
+# a = "qwerty"
+# b = "asdfgh"
+
+# i = 0
+# while i < len(a):
+#     print(a[i] + b[i])
+#     i +=1
+
+
+# if 10>5 and 3<5:
+#     print("cool")
+
+# if 10>5 or 3<5:
+#     print("cool")
+
+# i = 0
+# while i<10:
+#     print(i)
+#     i +=1
+#     if i == 5:
+#         break
+
+# i = 0
+# while i<5:
+#     i+=1
+#     if i ==3:
+     
+
+full_name = input("enter full name here: ")
+
+i = 0
+
+while i < 13:
+    if full_name[i] in "aeiou":
+        print(str(i) + " - " + full_name[i])
+
+    i +=1
+
+    
+   
+
+ 
+
+
